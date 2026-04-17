f373fca3b36e034e613e3b0495168fb56b321800
yyh174 <83095935@qq.com>
2026-04-17 18:38:02 +0800

    初始化

---
+++ src/test/java/com/xl/can/CanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xl.can;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
